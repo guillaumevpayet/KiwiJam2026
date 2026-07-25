a00f615c7956112eefbc6058b68f3409f3fce9e8
mrgw4 <34224752+mrgw4@users.noreply.github.com>
2026-07-25 13:37:36 +1200

    Feat: Setup HUD and drian
    - Setup player HUD with progress bar and health points,
    - Setup ability to increment and decrement hp.
    - Setup passive hp drain.

---
+++ Assets/UI Toolkit/UIScripts/PlayerHud.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerHud : MonoBehaviour
+{
+    public UIDocument UIDoc;
+    public int hpDrainInterval;
+    private List<Image> hpPips = new List<Image>();
+    private int hpLeft = 5;
+    VisualElement root;
+
+    private void OnEnable()
+    {
+        root = UIDoc.rootVisualElement;
+    }
+    private void Start()
+    {
+        hpPips.Add(root.Q("Pip1") as Image);
+        hpPips.Add(root.Q<Image>("Pip2"));
+        hpPips.Add(root.Q<Image>("Pip3"));
+        hpPips.Add(root.Q<Image>("Pip4"));
+        hpPips.Add(root.Q<Image>("Pip5"));
+        InvokeRepeating(nameof(HealthDrain),hpDrainInterval,hpDrainInterval); 
+    }
+
+    private void HealthDrain()
+    {
+        HealthChanged(false);
+    }
+
+
+    void HealthChanged(bool direction)
+    {
+        if (direction)
+        {
+            if (hpLeft < 5)
+            {
+                hpPips[hpLeft].visible = true;
+                hpLeft += 1;
+            }
+        }
+        else
+        {
+            if (hpLeft <= 1)
+            {
+                Debug.Log("Player Dead!!!!!");
+            }
+            else
+            {
+                Debug.Log("PANIC");
+                hpLeft -= 1;
+                hpPips[hpLeft].visible = false;
+            }
+        }
+    }
+
+
+}
